bdd95123470e481d23daf5b9f1828fd8121eecff
Bibit Bianchini <ebianchini.mit@gmail.com>
2025-10-17 16:51:39 -0400

    Include source for computing Hessian of squared angle error between two quaternions

---
+++ systems/common/quaternion_error_hessian.h
@@ -0,0 +1,27 @@
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include "drake/common/drake_assert.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+
+
+namespace c3 {
+namespace systems {
+namespace common {
+
+/*!
+ * Computes the Hessian of the squared angle difference between two quaternions,
+ * with respect to the elements of the first.
+ * @param quat The first quaternion (size 4 vector).  The Hessian of the squared
+ * angle difference is with respect to this quaternion.
+ * @param quat_desired The second quaternion (size 4 vector).
+ * @return The 4x4 Hessian matrix of the squared angle difference.
+*/
+ Eigen::MatrixXd hessian_of_squared_quaternion_angle_difference(
+    const Eigen::VectorXd& quat,
+    const Eigen::VectorXd& quat_desired);
+
+} // namespace common
+} // namespace systems
+} // namespace c3
